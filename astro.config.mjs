c70865971db7a0788d9420526525d4998aec7cb2
gptkong <gptkong>
2025-06-13 11:00:56 +0800

    add shiki config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://oaico.de',
+  site: "https://oaico.de",
   integrations: [mdx(), sitemap(), react()],
 
   vite: {
     plugins: [tailwindcss()],
+  },
+  markdown: {
+    shikiConfig: {
+      wrap: false,
+    },
   },
 });
