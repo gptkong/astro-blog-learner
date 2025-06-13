5911952ccadad79b5e22b359719e9dcb587cc7a0
gptkong <gptkong>
2025-06-13 13:27:48 +0800

    code theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,6 +17,10 @@ export default defineConfig({
   },
   markdown: {
     shikiConfig: {
+      themes: {
+        light: "github-light",
+        dark: "github-dark",
+      },
       wrap: false,
     },
   },
